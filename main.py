c216f919c3a364cffcebd3cdef76623cb5faf199
BushiestBush <brett.jorgensen989@gmail.com>
2024-03-12 19:34:36 -0500

    built out additional functions for get_book_text, get_letter_count, and updated main to output report

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ def main():
     text = get_book_text(file_path)
     num_words = get_num_words(text)
     num_letters_dict = get_letter_count(text)
-    print(f"total word count: {num_words}")
-    
+    print(f"--- Begin report of {file_path} ---")
+    print(f"{num_words} words found in the document")
+    sorted_lst = get_output_string(num_letters_dict)
+    for s in sorted_lst:
+        print(f"The '{s[0]}' character was found {s[1]}")
+    print("--- End report ---")
+
 def get_num_words(text):
     # count of the number of words
     words_list = []
@@ -17,9 +22,29 @@ def get_book_text(file_path):
         return f.read()
 
 def get_letter_count(text):
-    # counts the number of letters
-    letters_dict = {}
+    # counts the number of unique characters
+    num_letters_dict = {}
     lowered_string = text.lower()
     words = lowered_string.split()
+    for word in words:
+        for i in word:
+            if i.isalpha():
+                if i in num_letters_dict:
+                    num_letters_dict[i] += 1
+                else:
+                    num_letters_dict[i] = 1
+            else:
+                pass
+    return num_letters_dict
+
+def sort_on(sorted_lst):
+    return sorted_lst[1]
+
+def get_output_string(num_letters_dict):
+    #returns the report to be printed for the program
+    sorted_lst = list(num_letters_dict.items())
+    sorted_lst.sort(reverse=True, key=sort_on)
+    return sorted_lst
+
 main()
 
